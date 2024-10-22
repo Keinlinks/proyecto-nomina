8d5dde774a6fc858135d57ec395fafca2ffff62e
Kevin <kevin.clift.b@gmail.com>
2024-10-21 23:22:55 -0300

    se muestran los sueldos liquidos y brutos

--- src/app/pages/calculate/calculate.component.ts
+++ src/app/pages/calculate/calculate.component.ts
@@ -1,9 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { HttpClient } from '@angular/common/http';
 @Component({
@@ -16,17 +16,29 @@ import { HttpClient } from '@angular/common/http';
 })
 export class CalculateComponent {
   http = inject(HttpClient);
+  cd = inject(ChangeDetectorRef);
   nominalSalaryResult: string = '';
+  salaryLiquid = 0;
+  salaryBruto = 0;
   nominalSalaryForm = new FormGroup({
-    salary: new FormControl(0),
-    hours: new FormControl(0),
-    bonus: new FormControl(0),
-    commission: new FormControl(0),
-    afp: new FormControl(''),
+    salary: new FormControl(0, [Validators.required]),
+    hours: new FormControl(0, [Validators.required]),
+    bonus: new FormControl(0, [Validators.required]),
+    commission: new FormControl(0, [Validators.required]),
+    afp: new FormControl('', [Validators.required]),
   });
   calculateNominal(){
-    this.http.post<string>('nominalSalary',this.nominalSalaryForm.getRawValue()).subscribe((result)=>{
-      this.nominalSalaryResult = result;
-    })
+    if (this.nominalSalaryForm && this.nominalSalaryForm.invalid) return;
+    this.http
+      .post<string>(
+        'calculator/nominaSalary',
+        this.nominalSalaryForm.getRawValue()
+      )
+      .subscribe((result) => {
+        let form:any = this.nominalSalaryForm.getRawValue();
+        this.salaryBruto = form.salary * form.hours + form.bonus;
+        this.salaryLiquid = +result;
+        this.cd.detectChanges();
+      });
   }
 }
